335071bc6ca6a3356c8693e17f7c5bc67ca7ef27
Spoorthy <kasojuspoorthy@gmail.com>
2025-09-07 11:57:55 +0530

    Update .gitignore, app.py, and requirements

--- app.py
+++ app.py
@@ -85,4 +85,5 @@ def upload_file():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
